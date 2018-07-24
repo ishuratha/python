aab5e08ed8ec1792227ed13beef75e5e3b516561
ishuratha <ishwariyat23@gmail.com>
2018-07-24 16:05:43 +0530

    Create low high.c

---
+++ low high.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a[50],size,low,high;
+printf("\n Enter the size of the array");
+scanf("%d",&size);
+printf("\n Enter %d elements into array:",size);
+for(i=0;i<size;i++)
+scanf("%d",&a[i]);
+high a[0];
+for(i=1;i<size;i++)
+if(big<a[i])
+big=a[i];
+}
+printf("\n largest element:%d",high);
+low=a[0];
+for(i=1;i<size;i++)
+if(low>a[i])
+low=a[i];
+}
+printf("smallest element:%d",low);
+return 0;
+}
