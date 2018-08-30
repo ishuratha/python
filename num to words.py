5e41721bcfbf9462e1610482b1f25e3d2bdc3429
ishuratha <ishwariyat23@gmail.com>
2018-08-30 15:27:31 +0530

    Create num to words.py

---
+++ num to words.py
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(void)
+{
+int num;
+printf("\n Enter the number from 1 to 5);
+scanf("%d",&num);
+switch(num)
+{
+case1:
+printf("\nOne");
+break;
+case2:
+printf("\nTwo");
+break;
+case3:
+printf("\nThree");
+break;
+case4:
+printf("\nFour");
+break;
+case5:
+printf("\nFive");
+break;
+default;
+print("Enter the number from 1 to 5");
+}
+return 0;
+}
