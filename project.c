ee38d906c9be74317defef186c93726a52b5cf97
ishuratha <ishwariyat23@gmail.com>
2018-09-03 15:30:18 +0530

    Create project.c

---
+++ project.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s1,s2,k;
+printf("Enter the two number");
+scanf("%d%d",&s1,&s2);
+k=s1*s2;
+if(k*2==0)
+{
+printf("Even");
+}
+else
+{
+printf("odd");
+}
+}
